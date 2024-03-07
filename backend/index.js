4a7be330b807ad97d9e4ab468b5012ec406fcb57
denis daros <denisv.daros@gmail.com>
2024-03-07 00:05:01 -0300

    vercel

--- backend/index.js
+++ backend/index.js
@@ -10,4 +10,4 @@ const loginRouter = require('./src/routes/login.router');
 
 app.use(loginRouter)
 
-app.listen(3001, () => console.log("Server is running on port 3001"))
+app.listen(process.env.PORT, () => console.log("Server is running on port 3001"))
